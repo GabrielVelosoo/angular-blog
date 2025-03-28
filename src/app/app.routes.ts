90b2f3533a74f30e07007ae99f8b4b9edc7dfe74
Gabriel Veloso Pinheiro <tr26660es@trf1.gov.br>
2025-03-28 16:12:41 -0300

    develop: Entendendo Guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,25 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from './pages/home/home.component';
-import {ContentComponent} from './pages/content/content.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContentComponent } from './pages/content/content.component';
+import { authGuard } from './guards/auth.guard';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   },
   {
     path: 'content/:id',
     component: ContentComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: '**',
+    redirectTo: ''
   }
 ];
